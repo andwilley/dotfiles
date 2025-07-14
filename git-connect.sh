7b42a44ad2d524f227efd10e8c402b466a0aaac3
Drew Willey <andwilley@gmail.com>
2025-07-14 22:29:31 +0000

    generate ssh config

--- git-connect.sh
+++ git-connect.sh
@@ -1,3 +1,8 @@
 gh auth refresh -h github.com -s admin:public_key
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/github_rsa -N ""
-gh ssh-key add ~/.ssh/github_rsa.pub -t "cloud-dev-env-$(date +%s)"
+ssh-keygen -t ed25519 -f ~/.ssh/github_key -N ""
+touch ~/.ssh/config
+cat > ~/.ssh/config << EOM
+Host github.com
+    IdentityFile ~/.ssh/github_key
+EOM
+gh ssh-key add ~/.ssh/github_key.pub -t "cloud-dev-env-$(date +%s)"
